1e1322eba71ee227c84aca3cbfec8f9a9e0c562a
Mishail <misha.123qq@ya.ru>
2025-03-15 21:14:24 +0300

    Убрал ненужные пробелы(3)

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -4,7 +4,9 @@ import interfaces.HistoryManager;
 import interfaces.TaskManager;
 
 public class Managers {
-    private Managers() {}
+    private Managers() {
+
+    }
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
